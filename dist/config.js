c97c26b2fba47ddf278b91c096a2d521f71859a5
TellyXu <xutelly@gmail.com>
2024-10-22 15:54:38 -0400

    build

--- dist/config.js
+++ dist/config.js
@@ -2,72 +2,72 @@ const Um = {
     logoUrl: "https://img.picui.cn/free/2024/09/26/66f57338841ea.png",
     title: "Cancer Health Equity + AI/ML Lab supported by AIM-AHEAD Program for AI Readiness (PAIR)",
     bannerUrls: [
-      "https://i.ibb.co/pv0GS05/Dr-George-with-her-students-Christine-Cho-and-Gianni-Pucillo.jpg",
-      "https://i.ibb.co/tzTNzDp/SDSU-President-and-her-entourage-visits-Dr-George-Lab.jpg",
-      "https://i.ibb.co/pv0GS05/Dr-George-with-her-students-Christine-Cho-and-Gianni-Pucillo.jpg",
-      "https://i.ibb.co/tzTNzDp/SDSU-President-and-her-entourage-visits-Dr-George-Lab.jpg",
-      "https://i.ibb.co/pv0GS05/Dr-George-with-her-students-Christine-Cho-and-Gianni-Pucillo.jpg"
+        "https://i.ibb.co/pv0GS05/Dr-George-with-her-students-Christine-Cho-and-Gianni-Pucillo.jpg",
+        "https://i.ibb.co/tzTNzDp/SDSU-President-and-her-entourage-visits-Dr-George-Lab.jpg",
+        "https://i.ibb.co/pv0GS05/Dr-George-with-her-students-Christine-Cho-and-Gianni-Pucillo.jpg",
+        "https://i.ibb.co/tzTNzDp/SDSU-President-and-her-entourage-visits-Dr-George-Lab.jpg",
+        "https://i.ibb.co/pv0GS05/Dr-George-with-her-students-Christine-Cho-and-Gianni-Pucillo.jpg"
     ],
     main: {
-      titlePrimary: "Cancer Health Equity + AI/ML Lab",
-      titleSecondary: "supported by AIM-AHEAD Program for AI Readiness (PAIR)"
+        titlePrimary: "Cancer Health Equity + AI/ML Lab",
+        titleSecondary: "supported by AIM-AHEAD Program for AI Readiness (PAIR)"
     }
-  };
-  
-  const $m = {
+};
+
+const $m = {
     regionOne: {
-      title: "The Latest",
-      left: {
-        text: "Cancer Health Equity + AI/ML Lab supported by AIM-AHEAD Program for AI Readiness (PAIR)",
-        textSubsidiary: "TBD"
-      },
-      right: {
-        one: {
-          "text-1-1": "10+",
-          "text-1-2": "Courses Offered",
-          "text-1-3": "Our Courses on AIM-AHEAD"
+        title: "The Latest",
+        left: {
+            text: "Cancer Health Equity + AI/ML Lab supported by AIM-AHEAD Program for AI Readiness (PAIR)",
+            textSubsidiary: "TBD"
         },
-        two: {
-          "text-2-1": "10+",
-          "text-2-2": "Partner Organizations",
-          "text-2-3": "We collaborate with a vast array of partners across all states and union territories"
-        },
-        three: {
-          "text-3-1": "100+",
-          "text-3-2": "San Diego State University",
-          "text-3-3": "Our multidisciplinary faculty are committed to advancing AIMAHEAD"
-        },
-        GoogleScholarLink: "https://scholar.google.com",
-        moreBtnLink: "https://www.google.com"
-      }
+        right: {
+            one: {
+                "text-1-1": "10+",
+                "text-1-2": "Courses Offered",
+                "text-1-3": "Our Courses on AIM-AHEAD"
+            },
+            two: {
+                "text-2-1": "10+",
+                "text-2-2": "Partner Organizations",
+                "text-2-3": "We collaborate with a vast array of partners across all states and union territories"
+            },
+            three: {
+                "text-3-1": "100+",
+                "text-3-2": "San Diego State University",
+                "text-3-3": "Our multidisciplinary faculty are committed to advancing AIMAHEAD"
+            },
+            GoogleScholarLink: "https://scholar.google.com",
+            moreBtnLink: "https://www.google.com"
+        }
     },
     regionTwo: {
-      resources: {
-        link: "https://www.google.com/search?q=Resources",
-        introducePrimary: "Read the latest research news, learn about our affiliated centers and institutes, and access resources for conducting research.",
-        introduceSecondary: "Resources for Researchers",
-        introduceSecondaryLink: "https://www.google.com/search?q=introduceSecondaryLink"
-      },
-      education: {
-        link: "https://www.google.com/search?q=education",
-        introducePrimary: "Discover student opportunities and connections."
-      }
+        resources: {
+            link: "https://www.google.com/search?q=Resources",
+            introducePrimary: "Read the latest research news, learn about our affiliated centers and institutes, and access resources for conducting research.",
+            introduceSecondary: "Resources for Researchers",
+            introduceSecondaryLink: "https://www.google.com/search?q=introduceSecondaryLink"
+        },
+        education: {
+            link: "https://www.google.com/search?q=education",
+            introducePrimary: "Discover student opportunities and connections."
+        }
     },
     regionThree: {
-      titlePrimary: "Cancer Health Equity + AI/ML Lab supported by Program for AI Readiness(PAIR)",
-      titleSecondary: "JOIN THE EMAIL LIST",
-      buttonText: "Join Now",
-      joinNowLink: "https://www.google.com/search?q=joinNowLink"
+        titlePrimary: "Cancer Health Equity + AI/ML Lab supported by Program for AI Readiness(PAIR)",
+        titleSecondary: "JOIN THE EMAIL LIST",
+        buttonText: "Join Now",
+        joinNowLink: "https://www.google.com/search?q=joinNowLink"
     },
     regionFour: {
-      titlePrimary: "Join Us",
-      titleSecondary: "Join Our Lab, Changing the World",
-      buttonText: "Join Us Now",
-      joinUsNowLink: "https://www.google.com/search?q=joinUsNowLink"
+        titlePrimary: "Join Us",
+        titleSecondary: "Join Our Lab, Changing the World",
+        buttonText: "Join Us Now",
+        joinUsNowLink: "https://www.google.com/search?q=joinUsNowLink"
     }
-  };
-  
-  const Wm = {
+};
+
+const Wm = {
     logoUrl: "https://img.picui.cn/free/2024/09/26/66f57338841ea.png",
     list1: "CONTACT US",
     list1Link: "https://www.google.com/search?q=list1Link",
@@ -76,11 +76,12 @@ const Um = {
     list3: "SAN DIEGO STATE UNIVERSITY",
     list3Link: "https://www.google.com/search?q=list3Link",
     copyright: "AIM-AHEAD Coordinating Center is funded by NIH, Agreement No.: 1OT2OD032581-01"
-  };
-  
-  const L = {
+};
+
+const L = {
     header: Um,
     main: $m,
     footer: Wm
-  };
-  
+};
+
+export default L;
