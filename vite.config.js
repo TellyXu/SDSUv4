0972433a28b2f6b8874bce373cdcf21ed86cd839
TellyXu <xutelly@gmail.com>
2024-11-13 12:01:06 -0500

    instructions

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',  // Adjust if deployed in a subdirectory, or leave as "/" for root
   plugins: [react()],
 
   server: {
-
     proxy: {
       '/dev': {
         target: 'http://127.0.0.1:5000/',
@@ -14,4 +14,4 @@ export default defineConfig({
       },
     }
   }
-})
+});
